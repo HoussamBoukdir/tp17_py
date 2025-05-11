c02846224b9ec1d1a307457207f08554f3dce7a4
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-05-12 00:56:46 +0100

    Add files via upload

---
+++ ex3-1.py
@@ -0,0 +1,14 @@
+def calcul(n1=10,n2=12):
+    calcul(n1=10,n2=12)
+cn1=int(input("veuiller saisir le premier coefficient:"))  
+cn2=int(input("veuiller saisir le second coefficient:")) 
+n1=float
+n2=float   
+not1=cn1*n1
+not2=cn2*n2
+print("not_1 est:",not1)
+print("not_2 est:",not2)
+moy=(not1 + not2)/(cn1+cn2)
+if not1>0 and not2>0 and not1<20 and not2<20 and cn1>0 and cn2>0 :
+    print("la moy est ",moy)
+    calcul(n1=10, n2=12)
